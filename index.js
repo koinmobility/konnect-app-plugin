d6c4c23999b764a6935307d37371f3f71c5930d5
ChrisjayPedra <chrisjay.pedra.xentra@gmail.com>
2024-09-13 22:57:01 +0800

    update

--- index.js
+++ index.js
@@ -1,31 +1,13 @@
 
-
-const { CapacitorHttp } = require('@capacitor/core'); 
 const axios = require('axios');
 
-function test_login_2(mobileNumber, password, loginType) {
-  const headers = {
-    ProgramKey: 'ARDENTQA2023',
-    ApiKey: 'ARDENTQA2023PROP',
-    'Content-Type': 'application/json',
-    Authorization: 'Basic YWRtaW46QGRtMW4xMjM0',
-  };
-
-  console.log('input', loginType, password, mobileNumber);
-
-  const body = {
-    MobileNumber: mobileNumber,
-    Password: password,
-    LoginType: loginType,
-  };
-
-  const url = 'https://kmcore-dev.koinmobility.com/KOIN/Login';
-
+function test_login(headers,body) {
+  const url = 'https://kmcore-dev.koinmobility.com/KOIN/Login'
   return new Promise((resolve, reject) => {
     axios.post(url, body, { headers })
       .then((res) => {
-        console.log('result', res.data); // Access the response data
-        resolve(res.data); // Resolve with the response data
+        console.log('result', res.data); 
+        resolve(res.data);
       })
       .catch((error) => {
         console.error('HTTP Error:', error);
@@ -34,58 +16,62 @@ function test_login_2(mobileNumber, password, loginType) {
   });
 }
 
-function test_login(username, password) {
-  const options = {
-    method: 'GET',
-    url: this.url,
-    params: { P01: username, P02: password, method: 'login' },
-  };
 
-  return new Promise((resolve, reject) => {
-    CapacitorHttp.get(options).then(
-      (res) => {
-        console.log('result', res);
-        resolve(res);
-      },
-      (error) => {
-        console.error('HTTP Error:', error);
-        reject(error);
-      }
-    );
-  });
-}
 
-function testCall(data) {
-  return `Hello, ${data}!`;
-}
 
-function login(username, password) {
+
+
+
+
+
+
+
+
+
+// function test_login(username, password) {
+//   const options = {
+//     method: 'GET',
+//     url: this.url,
+//     params: { P01: username, P02: password, method: 'login' },
+//   };
+
+//   return new Promise((resolve, reject) => {
+//     CapacitorHttp.get(options).then(
+//       (res) => {
+//         console.log('result', res);
+//         resolve(res);
+//       },
+//       (error) => {
+//         console.error('HTTP Error:', error);
+//         reject(error);
+//       }
+//     );
+//   });
+// }
+// function testCall(data) {
+//   return `Hello, ${data}!`;
+// }
+// function login(username, password) {
   
-  return new Promise((resolve, reject) => {
-    if (username === 'admin' && password === 'password') {
-      resolve( 'Login successful!');
-    } else {
-      reject( 'Invalid credentials');
-    }
-  });
+//   return new Promise((resolve, reject) => {
+//     if (username === 'admin' && password === 'password') {
+//       resolve( 'Login successful!');
+//     } else {
+//       reject( 'Invalid credentials');
+//     }
+//   });
 
-}
-
-function logout() {
+// }
+// function logout() {
   
-  return 'Logged out successfully';
-}
-
-function register(username, password) {
+//   return 'Logged out successfully';
+// }
+// function register(username, password) {
  
-  return `User ${username} registered successfully!`;
-}
-
+//   return `User ${username} registered successfully!`;
+// }
 module.exports = {
-  testCall,
+ 
   test_login,
-  test_login_2,
-  login,
-  logout,
-  register,
+  
 };
